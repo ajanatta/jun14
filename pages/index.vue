ce35a50aa342242981f826f62aded08d8432e07c
Athishay <athishay@anattadesign.com>
2022-06-14 22:14:48 +0530

    Fetching data

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <Tutorial/>
+  <div class="m-5">
+
+  <div class="flex justify-center">
+    Hello!
+  </div>
+  
+</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async asyncData({ $prismic, error }) {
+    try {
+      const offerSection = (await $prismic.api.getSingle('offer-section')).data;
+      console.log(offerSection);
+      return { 
+        offerSection 
+      } 
+    } catch {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  }
 }
 </script>
-
-<style>
-  html {
-    font-family: "Oxygen";
-  }
-</style>
