8d4a6b935aef69c68c91ba50d000b372994c816c
Athishay <athishay@anattadesign.com>
2022-06-15 20:17:24 +0530

    VWO

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,10 +25,6 @@ export default {
     ],
     scripts: [
       {
-        src: "https://dev.visualwebsiteoptimizer.com/lib/630183.js",
-        mode: "client"
-      },
-      {
         src: "https://static.cdn.prismic.io/prismic.js?new=true&repo=juntwo",
       },
     ]
