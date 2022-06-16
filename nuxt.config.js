9a5e57e8ea505e931e5aff06ebb7cecf7b1f4647
Athishay <athishay@anattadesign.com>
2022-06-16 15:48:31 +0530

    WVO

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,10 +37,10 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    {
-      src: "~/assets/js/630183.js",
-      mode: "client"
-    }
+    // {
+    //   src: "~/assets/js/630183.js",
+    //   mode: "client"
+    // }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
