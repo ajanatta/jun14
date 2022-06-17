ef9bf335d4436054a123c0567bbd6be93152ca18
Athishay <athishay@anattadesign.com>
2022-06-17 14:40:03 +0530

    VWO Callback

--- layout/default.vue
+++ layout/default.vue
@@ -2,6 +2,23 @@
   <Nuxt />
 </template>
 
+<script>
+
+export default {
+  mounted() {
+    if(process.client) {
+      window.VWO.push([
+          'onVariationApplied',
+          (data) => {
+            console.log(data);
+          }
+      ]);
+    }
+
+  }
+}
+
+</script>
 
 <style>
 
