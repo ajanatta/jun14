08ec91cf1ab1f668b74b1e14d445b6de500a1c29
Athishay <athishay@anattadesign.com>
2022-06-17 10:18:54 +0530

    VWO Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     {
-      src: "~/assets/js/630183.js",
+      src: "~/assets/js/630582.js",
       mode: "client"
     }
   ],
